ae1934239184d0db19f4efcb5fd257bdd17c4419
root <jhonatheberson@gmail.com>
2022-07-06 20:36:09 -0300

    style(cleancode): refactory in Program.cs
    added class, and methods

--- acesso_dados_com_dot_net/Program.cs
+++ acesso_dados_com_dot_net/Program.cs
@@ -2,55 +2,80 @@
 using Dapper;
 using Microsoft.Data.SqlClient;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-const string connectionString = "Server=worldofai.database.windows.net;Database=balta;User ID=jhonatheberson;Password=BLAZEjoao55@#";
 
-var student = new Student();
-student.Id = Guid.NewGuid();
-student.Name = "jhonat";
-student.Email = "jhonat@gmail.com";
-student.Phone = "84976479384";
-student.Birthdate = DateTime.Parse("2/16/2008 12:15:12 PM");
-student.CreateDate = DateTime.Now;
-
-// NÃO VAMOS CONCATENAR STRING EM SQL, PARA NÃO OCORRER SQL INJECTION
-// O "@" FAZ QUE EU POSSA PELAS AS LINHAS
-// O "$" FAZ QUE NÃO POSSA CONCATENAR
-// VAMOS OPTAR POR RECEBER PARÂMETROS E RECEBE PARA METROS PELO "@"
-var insertSql = @"INSERT INTO 
-  [Student] 
-VALUES(
-  @Id,
-  @Nome,
-  @Email,
-  @Document,
-  @Phone,
-  @Birthdate,
-  @CreateDate)"; //vamos evitar comandos SQL como NEWID(), no C#
-
-using (var connection = new SqlConnection(connectionString))
+namespace BaltaDataAccess
 {
-    // EVITAR PROCESSAMENTO AQUI DENTRO, PORQUE A CONEXÃO ESTÁ ABERTA
-
-    //UTILIZANDO PARAMETROS PARA MONTAR SQL EXECUTE
-    var rows = connection.Execute(insertSql, new {
-      student.Id,
-      Nome = student.Name,
-      student.Email,
-      student.Document,
-      student.Phone,
-      student.Birthdate,
-      student.CreateDate
-    });
-
-    Console.WriteLine($"{rows} linhas inderidas");
-
-    var students = connection.Query<Student>("SELECT TOP (1000) [Id],[Name],[Email],[Document],[Phone],[Birthdate],[CreateDate]FROM[dbo].[Student]");
-    foreach (var item in students)
+  class Program
+  {
+    static void Main(string[] args)
     {
-      Console.WriteLine($"{item.Id} - {item.Name}");
+
+      const string connectionString = "Server=worldofai.database.windows.net;Database=balta;User ID=jhonatheberson;Password=BLAZEjoao55@#";
+
+
+      using (var connection = new SqlConnection(connectionString))
+      {
+        // EVITAR PROCESSAMENTO AQUI DENTRO, PORQUE A CONEXÃO ESTÁ ABERTA
+
+        ListStudents(connection);
+        // CreateStudent(connection);
+
+
+      }
+
     }
+    static void ListStudents(SqlConnection connection)
+    {
+
+      var students = connection.Query<Student>("SELECT TOP (1000) [Id],[Name],[Email],[Document],[Phone],[Birthdate],[CreateDate]FROM[dbo].[Student]");
+      foreach (var item in students)
+      {
+        Console.WriteLine($"{item.Id} - {item.Name}");
+      }
+
+    }
+    static void CreateStudent(SqlConnection connection)
+    {
+      var student = new Student();
+      student.Id = Guid.NewGuid();
+      student.Name = "jhonat";
+      student.Email = "jhonat@gmail.com";
+      student.Phone = "84976479384";
+      student.Birthdate = DateTime.Parse("2/16/2008 12:15:12 PM");
+      student.CreateDate = DateTime.Now;
+
+      // NÃO VAMOS CONCATENAR STRING EM SQL, PARA NÃO OCORRER SQL INJECTION
+      // O "@" FAZ QUE EU POSSA PELAS AS LINHAS
+      // O "$" FAZ QUE NÃO POSSA CONCATENAR
+      // VAMOS OPTAR POR RECEBER PARÂMETROS E RECEBE PARA METROS PELO "@"
+      var insertSql = @"INSERT INTO 
+        [Student] 
+      VALUES(
+        @Id,
+        @Nome,
+        @Email,
+        @Document,
+        @Phone,
+        @Birthdate,
+        @CreateDate)"; //vamos evitar comandos SQL como NEWID(), no C#
+
+
+      //UTILIZANDO PARAMETROS PARA MONTAR SQL EXECUTE
+      var rows = connection.Execute(insertSql, new
+      {
+        student.Id,
+        Nome = student.Name,
+        student.Email,
+        student.Document,
+        student.Phone,
+        student.Birthdate,
+        student.CreateDate
+      });
+
+      Console.WriteLine($"{rows} linhas inderidas");
+    }
+  }
+
 }
 
--- acesso_dados_com_dot_net/README.md
+++ acesso_dados_com_dot_net/README.md
@@ -265,3 +265,92 @@ agora que criamos instalamaos o pacote, vamos criar nosso modelo.
   ~~~
 
   O metodo **Execute**  vai ser utilizado tanto para **INSERT**, **UPDATE** e **DELETE**
+
+
+## UPDATE
+
+  - antes de iniciar o update, vamos refatorar o codigo, deixando mais modular e limpando de forma mais clara, então ficamos assim:
+
+
+   ~~~cs
+  using BaltaDataAcces.Models;
+  using Dapper;
+  using Microsoft.Data.SqlClient;
+
+
+
+  namespace BaltaDataAccess
+  {
+    class Program
+    {
+      static void Main(string[] args)
+      {
+
+        const string connectionString = "Server=worldofai.database.windows.net;Database=balta;User ID=jhonatheberson;Password=BLAZEjoao55@#";
+
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+          // EVITAR PROCESSAMENTO AQUI DENTRO, PORQUE A CONEXÃO ESTÁ ABERTA
+
+          ListStudents(connection);
+          // CreateStudent(connection);
+
+
+        }
+
+      }
+      static void ListStudents(SqlConnection connection)
+      {
+
+        var students = connection.Query<Student>("SELECT TOP (1000) [Id],[Name],[Email],[Document],[Phone],[Birthdate],[CreateDate]FROM[dbo].[Student]");
+        foreach (var item in students)
+        {
+          Console.WriteLine($"{item.Id} - {item.Name}");
+        }
+
+      }
+      static void CreateStudent(SqlConnection connection)
+      {
+        var student = new Student();
+        student.Id = Guid.NewGuid();
+        student.Name = "jhonat";
+        student.Email = "jhonat@gmail.com";
+        student.Phone = "84976479384";
+        student.Birthdate = DateTime.Parse("2/16/2008 12:15:12 PM");
+        student.CreateDate = DateTime.Now;
+
+        // NÃO VAMOS CONCATENAR STRING EM SQL, PARA NÃO OCORRER SQL INJECTION
+        // O "@" FAZ QUE EU POSSA PELAS AS LINHAS
+        // O "$" FAZ QUE NÃO POSSA CONCATENAR
+        // VAMOS OPTAR POR RECEBER PARÂMETROS E RECEBE PARA METROS PELO "@"
+        var insertSql = @"INSERT INTO 
+          [Student] 
+        VALUES(
+          @Id,
+          @Nome,
+          @Email,
+          @Document,
+          @Phone,
+          @Birthdate,
+          @CreateDate)"; //vamos evitar comandos SQL como NEWID(), no C#
+
+
+        //UTILIZANDO PARAMETROS PARA MONTAR SQL EXECUTE
+        var rows = connection.Execute(insertSql, new
+        {
+          student.Id,
+          Nome = student.Name,
+          student.Email,
+          student.Document,
+          student.Phone,
+          student.Birthdate,
+          student.CreateDate
+        });
+
+        Console.WriteLine($"{rows} linhas inderidas");
+      }
+    }
+
+  }
+   ~~~
